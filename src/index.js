41e967327a85d0012798c7437f24dd812ea15802
hassaan haider <hassaanhaider088@gmail.com>
2025-07-14 06:37:15 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const globalStore = new Map();
+
+export function useGlobalState(key, initialValue) {
+  if (!globalStore.has(key)) {
+    globalStore.set(key, {
+      value: initialValue,
+      listeners: new Set(),
+    });
+  }
+
+  const [, forceRender] = useState(0);
+
+  useEffect(() => {
+    const store = globalStore.get(key);
+    const update = () => forceRender(n => n + 1);
+    store.listeners.add(update);
+
+    return () => {
+      store.listeners.delete(update);
+    };
+  }, [key]);
+
+  const store = globalStore.get(key);
+
+  const setValue = (val) => {
+    store.value = typeof val === "function" ? val(store.value) : val;
+    store.listeners.forEach(listener => listener());
+  };
+
+  return [store.value, setValue];
+}
